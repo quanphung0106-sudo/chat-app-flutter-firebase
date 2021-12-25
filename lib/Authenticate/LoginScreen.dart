68269b13d685ac9392570a82ce82a92d989fd20d
Bao Cong <ngobaocong.it@gmail.com>
2021-12-25 11:38:36 +0700

    Cong commit Login: method and authenticate

---
+++ lib/Authenticate/LoginScreen.dart
@@ -0,0 +1,235 @@
+import 'package:chat_app/Authenticate/CreateAccount.dart';
+import 'package:chat_app/Screens/HomeScreen.dart';
+import 'package:chat_app/Authenticate/Methods.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+
+  bool _isVisible = false;
+
+  void updateStatus() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: isLoading
+          ? Center(
+              child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: size.height / 20,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      width: size.width / 0.5,
+                      child: IconButton(
+                          icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                    ),
+                    SizedBox(
+                      height: size.height / 50,
+                    ),
+                    Center(
+                      child: Text(
+                        "Welcom to Group 4",
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: Text(
+                        "Login to continue!",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      controller: _email,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                            .hasMatch(value)) {
+                          return 'Please validate email';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.email_sharp,
+                          size: 40,
+                        ),
+                        hintText: 'Your Email',
+                        labelText: "Email",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      obscureText: _isVisible ? false : true,
+                      controller: _password,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length <= 6) {
+                          return 'Password must has at lest 6 character!';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.password,
+                          size: 40,
+                        ),
+                        hintText: 'Your password',
+                        labelText: "Password",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 5.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: size.height / 20,
+                    ),
+                    customButton(size),
+                    SizedBox(
+                      height: size.height / 40,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (_) => CreateAccount())),
+                      child: Container(
+                        height: size.height / 14,
+                        width: size.width / 1.2,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.black),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Create Account",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+
+  Widget customButton(Size size) {
+    return GestureDetector(
+      onTap: () {
+        if (formKey.currentState!.validate()) {}
+        if (_email.text.isNotEmpty && _password.text.isNotEmpty) {
+          setState(() {
+            isLoading = true;
+          });
+
+          logIn(_email.text, _password.text).then((user) {
+            if (user != null) {
+              setState(() {
+                isLoading = false;
+              });
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => HomeScreen()));
+              return Fluttertoast.showToast(msg: "Login successful");
+            } else {
+              setState(() {
+                isLoading = false;
+              });
+              return Fluttertoast.showToast(msg: "Please check your email and password again!");
+            }
+          });
+        } else {
+          Fluttertoast.showToast(msg: "Please fill your information!");
+        }
+      },
+      child: Container(
+          height: size.height / 14,
+          width: size.width / 1.2,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.blue,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+    );
+  }
+
+  Widget field(
+      Size size, String hintText, IconData icon, TextEditingController cont) {
+    return Container(
+      height: size.height / 14,
+      width: size.width / 1.1,
+      child: TextField(
+        controller: cont,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
